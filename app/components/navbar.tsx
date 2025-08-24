d88f53b4576a5c0afbe2cc1f8a45725233584676
yao <63141491+yaonyan@users.noreply.github.com>
2025-08-24 23:20:55 +0800

    refactor: Remove search input from Navbar for cleaner layout

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,39 +7,16 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
 import { link as linkStyles } from "@heroui/theme";
 import clsx from "clsx";
 
-import { GithubIcon, SearchIcon, TwitterIcon } from "~/components/icons";
+import { GithubIcon, TwitterIcon } from "~/components/icons";
 import { siteConfig } from "~/config/site";
 import { useEffect, useState } from "react";
 import { ThemeSwitch } from "./theme-switch";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -90,7 +67,6 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -101,8 +77,7 @@ export const Navbar = () => {
         <NavbarMenuToggle />
       </NavbarContent>
 
-      <NavbarMenu>
-        {searchInput}
+  <NavbarMenu>
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
